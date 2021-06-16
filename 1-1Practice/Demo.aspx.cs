c0ec0c8bf11e3a554d770de196cde89b2ad2a96a
cc0321045 <adam0012367@gmail.com>
2021-06-17 00:24:54 +0800

    1-1 demo

--- 1-1Practice/Demo.aspx.cs
+++ 1-1Practice/Demo.aspx.cs
@@ -8,7 +8,7 @@ using System.Web.UI.WebControls;
 namespace _1_1Practice {
     public partial class Demo : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
-            Response.Write("Hi222");
+            Response.Write("Hi");
         }
     }
 }
